7d9d972ecb85678800a9220c448fe6a9e0b312d5
RajBaviskar <rajeshbaviskar.1987@gmail.com>
2017-07-31 15:37:28 -0500

    Update playNine.jsx

--- playNine.jsx
+++ playNine.jsx
@@ -13,50 +13,22 @@ const Stars = (props) => {
       )}
     </div>
   );
-};
+}
 
 const Button = (props) => {
-	let button;
-	switch(props.answerIsCorrect) {
-  	case true:
-    	button =
-      	<button className="btn btn-success" onClick={props.acceptAnswer}>
-          <i className="fa fa-check"></i>
-        </button>;
-    	break;
-    case false:
-    	button =
-    		<button className="btn btn-danger">
-          <i className="fa fa-times"></i>
-        </button>;
-    	break;
-    default:
-			button = 
-      	<button className="btn" 
-                onClick={props.checkAnswer}
-                disabled={props.selectedNumbers.length === 0}>
-        =
-        </button>;
-    break;
-  }
 	return(
-  	<div className="col-2 text-center">
-      {button}
-      <br /><br />
-      <button className="btn btn-warning btn-sm" onClick={props.redraw}
-              disabled={props.redraws==0}>
-        <i className="fa fa-refresh"></i> {props.redraws}
-      </button>
+  	<div className="col-2">
+      <button> = </button>
     </div>
   );
-};
+}
 
 const Answer = (props) => {
 	return(
   	<div className="col-5">
     {props.selectedNumbers.map((number, i) => 
     	<span key={i} onClick={() => props.unselectNumber(number)}>
-        {number}
+      {number}
       </span>
       )}
     </div>
@@ -65,13 +37,10 @@ const Answer = (props) => {
 
 const Numbers = (props) => {
 const numberClassName = (number) => {
-	if (props.usedNumbers.indexOf(number) >= 0) {
-  		return 'used';
-  	}
 	if (props.selectedNumbers.indexOf(number) >= 0) {
   		return 'selected';
   	}
-	};
+	}
 	return (
   	<div className="card text-center">
       <div>
@@ -88,115 +57,40 @@ const numberClassName = (number) => {
 
 Numbers.list = _.range(1, 10);
 
-const DoneFrame = (props) => {
-	return (
-  	<div className="text-center">
-      <h2> {props.doneStatus} </h2>
-    </div>
-  );
-}
-
 class Game extends React.Component {
-	static randomNumber = () => 1 + Math.floor(Math.random()*9);
 	state = {
   	selectedNumbers: [],
   	randomNumberOfStars: 1+ Math.floor(Math.random()*9),
-    usedNumbers: [],
-    answerIsCorrect: null,
-    redraws: 5,
-    doneStatus: null,
   };
   
   selectNumber = (clickedNumber) => {
   if (this.state.selectedNumbers.indexOf(clickedNumber) >= 0) {return;}
   	this.setState(prevState => ({
-    	answerIsCorrect: null,
     	selectedNumbers: prevState.selectedNumbers.concat(clickedNumber)
     }));
   };
   
   unselectNumber = (clickedNumber) => {
   	this.setState(prevState => ({
-    	answerIsCorrect: null,
     	selectedNumbers: prevState.selectedNumbers.filter(number => number!== clickedNumber)
     }));
   };
   
-  checkAnswer = () => {
-  	this.setState(prevState => ({
-    	answerIsCorrect: prevState.randomNumberOfStars ===
-      	prevState.selectedNumbers.reduce((acc, n) => acc + n, 0)
-    }));
-  };
-  
-  acceptAnswer = () => {
-  	this.setState(prevState => ({
-    	usedNumbers: prevState.usedNumbers.concat(prevState.selectedNumbers),
-      selectedNumbers: [],
-      answerIsCorrect: null,
-      randomNumberOfStars: 1+ Math.floor(Math.random()*9),
-    }));
-  };
-  
-  redraw = () => {
-  	if (this.state.redraws === 0) { return; }
-  	this.setState(prevState => ({
-    	randomNumberOfStars: 1 + Math.floor(Math.random()*9),
-      answerIsCorrect:null,
-      selectedNumbers:[],
-      redraws: prevState.redraws - 1,
-    }));
-  }
-  
-  possibleSolutions = (state) => {
-  	
-  };
-  
-  updateDoneStatus = () => {
-  	this.setstate(preState = {
-    	if (prevState.usedNumbers.length ===9) {
-      	return { doneStatus: 'Done. Nice!'};
-      }
-      if (prevState.redraws === 0 && !this.possibleSolutions(prevState)) {
-      	return { doneStatus: 'Game Over!'};
-      }
-    });
-  }
-  
 	render() {
-  	const {
-    	selectedNumbers,
-      randomNumberOfStars,
-      answerIsCorrect,
-      usedNumbers,
-      redraws,
-      doneStatus,
-    } = this.state;
-    
   	return (
     	<div className="container">
         <h3>Play Nine</h3> 
         <hr />
         <div className="row">
-          <Stars numberOfStars={randomNumberOfStars}/>
-          <Button selectedNumbers={selectedNumbers}
-                  redraws={redraws}
-                  checkAnswer={this.checkAnswer}
-                  acceptAnswer={this.acceptAnswer}
-                  redraw={this.redraw}
-                  answerIsCorrect={answerIsCorrect} />
-          <Answer selectedNumbers={selectedNumbers}
+          <Stars numberOfStars={this.state.randomNumberOfStars}/>
+          <Button />
+          <Answer selectedNumbers={this.state.selectedNumbers}
                   unselectNumber={this.unselectNumber}/>
         </div>
         <br />
-        {doneStatus ?
-        	<DoneFrame doneStatus={doneStatus} /> :
-        	<Numbers selectedNumbers={selectedNumbers} 
+        <Numbers selectedNumbers={this.state.selectedNumbers} 
                  selectNumber={this.selectNumber}
-                 usedNumbers={usedNumbers}
                  />
-        }
-        <br />
       </div>
     );
   }
